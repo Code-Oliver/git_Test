37cebf7bedeeda9030a788ac5d4917ca1cc0dec3
Oliver <Oliver@me.com>
2022-03-20 11:40:38 +0800

    hot-fix first

--- src/main/java/com/yby/git/GitTest1.java
+++ src/main/java/com/yby/git/GitTest1.java
@@ -3,5 +3,8 @@ package com.yby.git;
 public class GitTest1 {
     public static void main(String[] args) {
         System.out.println("GitTest1");
+        System.out.println("GitTest1");
+        System.out.println("GitTest1");
+        System.out.println("GitTest1");
     }
 }
